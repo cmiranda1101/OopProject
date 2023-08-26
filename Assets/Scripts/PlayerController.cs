d9594a643373386c0c4b76bf77b27c8ef77905de
cmiranda1101 <139823343+cmiranda1101@users.noreply.github.com>
2023-08-26 12:22:10 -0400

    Set up player
    Created code so the player could move and set up boundaries so they stay on screen

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,60 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private float speed = 20.0f;
+    public bool facingRight;
+    private float leftBound = -10.5f;
+    private float rightBound = 10.5f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        facingRight = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKey(KeyCode.D))
+        {
+            MoveRight();
+        }
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            MoveLeft();
+        }
+
+        if(transform.position.x <= leftBound) 
+        {
+            transform.position = new Vector3 (-10.5f, transform.position.y, transform.position.z);
+        }
+
+        if (transform.position.x >= rightBound)
+        {
+            transform.position = new Vector3 (10.5f, transform.position.y, transform.position.z);
+        }
+    }
+
+    void MoveRight()
+    {
+        if (facingRight == false)
+        {
+            transform.Rotate(0, 180, 0);
+            facingRight = true;
+        }
+        transform.Translate(Vector3.right * speed * Time.deltaTime);
+    }
+
+    void MoveLeft()
+    {
+        if (facingRight == true)
+        {
+            transform.Rotate(0, 180, 0);
+            facingRight = false;
+        }
+        transform.Translate(Vector3.right * speed * Time.deltaTime);
+    }
+}
